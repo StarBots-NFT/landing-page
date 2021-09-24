fbba0f702b5a3ffbae57431a9172d7a9d7354fac
truonggau <truongmaytinh1997@gmail.com>
2021-09-24 19:05:50 +0700

    bỏ thẻ meta tag

--- pages/index.tsx
+++ pages/index.tsx
@@ -412,8 +412,6 @@ function Home(props) {
         <>
             <Head>
                 <title>My Game | Landing Page</title>
-                <meta name="keywords" content="ninjas"/>
-                <meta name="viewport" content="width=1441, initial-scale=1.0"/>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"}/>
                 <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet"/>
