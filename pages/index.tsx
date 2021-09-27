a210456e9a3da076c5661a04522a0e3e81a8f584
truonggau <truongmaytinh1997@gmail.com>
2021-09-28 00:29:31 +0700

    test bug

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
-import gameIntroArt from "../public/Game Introduction.png"
 import stratege from '../public/Strategy 1.png'
+import gameIntroArt from "../public/Game Introduction.png"
 import weapons from '../public/Customisable Weapons.svg'
 import flexibility from '../public/Flexibility.svg'
 import playToEarn from '../public/Play to earn.svg'
