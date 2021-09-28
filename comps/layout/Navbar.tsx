de0bc823bf9334a81732d21260c9c06ab4378a33
truonggau <truongmaytinh1997@gmail.com>
2021-09-28 14:19:58 +0700

    fix bug nav

--- comps/layout/Navbar.tsx
+++ comps/layout/Navbar.tsx
@@ -34,7 +34,7 @@ const Navbar = () => {
             tablinks[newValue].style.color = "#00E1D2";
         } else {
             if (isShowDropDown) {
-               dropdownH = document.getElementById("dropdownBlock").getBoundingClientRect().height
+                dropdownH = document.getElementById("dropdownBlock").getBoundingClientRect().height
             }
             changeIsShow()
         }
@@ -103,17 +103,27 @@ const Navbar = () => {
             {isMobile ? (
                 <div className={styles.navbarMb} id={"navbar"}>
                     <div className={styles.navbarContent}>
-                        {!isBreakpoint ? (<img className={styles.img} src={"/logo_HEADER.png"}/>) : (<img className={styles.imgBreak} src={"/logo_HEADER.png"}/>)}
+                        {!isBreakpoint ? (<img className={styles.img} src={"/logo_HEADER.png"}/>) : (
+                            <img className={styles.imgBreak} src={"/logo_HEADER.png"}/>)}
                         {!isBreakpoint ? (
                             <div className={styles.content}>
                                 <div className={styles.tabs}>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 0)}>Home</button>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 1)}>Introduction</button>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 2)}>Trailer</button>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 3)}>Unique Feature</button>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 4)}>RoadMap</button>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 5)}>Sponsored</button>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 6)}>Our Team</button>
+                                    <button className="tablinks" onClick={(event) => handleChange(event, 0)}>Home
+                                    </button>
+                                    <button className="tablinks"
+                                            onClick={(event) => handleChange(event, 1)}>Introduction
+                                    </button>
+                                    <button className="tablinks" onClick={(event) => handleChange(event, 2)}>Trailer
+                                    </button>
+                                    <button className="tablinks" onClick={(event) => handleChange(event, 3)}>Unique
+                                        Feature
+                                    </button>
+                                    <button className="tablinks" onClick={(event) => handleChange(event, 4)}>RoadMap
+                                    </button>
+                                    <button className="tablinks" onClick={(event) => handleChange(event, 5)}>Sponsored
+                                    </button>
+                                    <button className="tablinks" onClick={(event) => handleChange(event, 6)}>Our Team
+                                    </button>
                                 </div>
                                 {/*<button className={styles.button}>*/}
                                 {/*    <LockIcon className={styles.lockIcon}/>*/}
@@ -121,23 +131,22 @@ const Navbar = () => {
                                 {/*</button>*/}
                             </div>
                         ) : (
-                            <div className={styles.contentBreak}>
-                                <menu className={styles.menu} onClick={changeIsShow}>
-                                    <div className={styles.menuDiv}></div>
-                                    <div className={styles.menuDiv}></div>
-                                    <div className={styles.menuDiv}></div>
-                                </menu>
-                            </div>
+                            <menu className={styles.menu} onClick={changeIsShow}>
+                                <div className={styles.menuDiv}></div>
+                                <div className={styles.menuDiv}></div>
+                                <div className={styles.menuDiv}></div>
+                            </menu>
                         )}
                         {(isShowDropDown && isBreakpoint) ? (
-                            <div className={styles.dropdownBlock} id = "dropdownBlock">
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,0)}>Home</div>
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,1)}>Introduction</div>
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,2)}>Trailer</div>
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,3)}>Unique Feature</div>
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,4)}>RoadMap</div>
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,5)}>Sponsored</div>
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,6)}>Our Team</div>
+                            <div className={styles.dropdownBlock} id="dropdownBlock">
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 0)}>Home</div>
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 1)}>Introduction</div>
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 2)}>Trailer</div>
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 3)}>Unique Feature
+                                </div>
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 4)}>RoadMap</div>
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 5)}>Sponsored</div>
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 6)}>Our Team</div>
                                 {/*<button className={styles.buttonDropDown}>*/}
                                 {/*    <LockIcon className={styles.lockIcon}/>*/}
                                 {/*    <span className={styles.buttonLabel}>Connect Wallet</span>*/}
@@ -150,17 +159,27 @@ const Navbar = () => {
             ) : (
                 <div className={styles.navbar} id={"navbar"}>
                     <div className={styles.navbarContent}>
-                        {!isBreakpoint ? (<img className={styles.img} src={"/logo_HEADER.png"}/>) : (<img className={styles.imgBreak} src={"/logo_HEADER.png"}/>)}
+                        {!isBreakpoint ? (<img className={styles.img} src={"/logo_HEADER.png"}/>) : (
+                            <img className={styles.imgBreak} src={"/logo_HEADER.png"}/>)}
                         {!isBreakpoint ? (
                             <div className={styles.content}>
                                 <div className={styles.tabs}>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 0)}>Home</button>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 1)}>Introduction</button>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 2)}>Trailer</button>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 3)}>Unique Feature</button>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 4)}>RoadMap</button>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 5)}>Sponsored</button>
-                                    <button className="tablinks" onClick={(event) => handleChange(event, 6)}>Our Team</button>
+                                    <button className="tablinks" onClick={(event) => handleChange(event, 0)}>Home
+                                    </button>
+                                    <button className="tablinks"
+                                            onClick={(event) => handleChange(event, 1)}>Introduction
+                                    </button>
+                                    <button className="tablinks" onClick={(event) => handleChange(event, 2)}>Trailer
+                                    </button>
+                                    <button className="tablinks" onClick={(event) => handleChange(event, 3)}>Unique
+                                        Feature
+                                    </button>
+                                    <button className="tablinks" onClick={(event) => handleChange(event, 4)}>RoadMap
+                                    </button>
+                                    <button className="tablinks" onClick={(event) => handleChange(event, 5)}>Sponsored
+                                    </button>
+                                    <button className="tablinks" onClick={(event) => handleChange(event, 6)}>Our Team
+                                    </button>
                                 </div>
                                 {/*<button className={styles.button}>*/}
                                 {/*    <LockIcon className={styles.lockIcon}/>*/}
@@ -168,23 +187,22 @@ const Navbar = () => {
                                 {/*</button>*/}
                             </div>
                         ) : (
-                            <div className={styles.contentBreak}>
-                                <menu className={styles.menu} onClick={changeIsShow}>
-                                    <div className={styles.menuDiv}></div>
-                                    <div className={styles.menuDiv}></div>
-                                    <div className={styles.menuDiv}></div>
-                                </menu>
-                            </div>
+                            <menu className={styles.menu} onClick={changeIsShow}>
+                                <div className={styles.menuDiv}></div>
+                                <div className={styles.menuDiv}></div>
+                                <div className={styles.menuDiv}></div>
+                            </menu>
                         )}
                         {(isShowDropDown && isBreakpoint) ? (
-                            <div className={styles.dropdownBlock} id = "dropdownBlock">
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,0)}>Home</div>
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,1)}>Introduction</div>
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,2)}>Trailer</div>
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,3)}>Unique Feature</div>
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,4)}>RoadMap</div>
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,5)}>Sponsored</div>
-                                <div className={styles.dropdown} onClick={(e)=>handleChange(e,6)}>Our Team</div>
+                            <div className={styles.dropdownBlock} id="dropdownBlock">
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 0)}>Home</div>
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 1)}>Introduction</div>
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 2)}>Trailer</div>
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 3)}>Unique Feature
+                                </div>
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 4)}>RoadMap</div>
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 5)}>Sponsored</div>
+                                <div className={styles.dropdown} onClick={(e) => handleChange(e, 6)}>Our Team</div>
                                 {/*<button className={styles.buttonDropDown}>*/}
                                 {/*    <LockIcon className={styles.lockIcon}/>*/}
                                 {/*    <span className={styles.buttonLabel}>Connect Wallet</span>*/}
