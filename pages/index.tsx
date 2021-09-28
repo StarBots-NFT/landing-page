82d663296106cf18bec06094a181512559f21f79
truonggau <truongmaytinh1997@gmail.com>
2021-09-28 17:04:20 +0700

    fix font chữ

--- pages/index.tsx
+++ pages/index.tsx
@@ -473,7 +473,6 @@ function Home(props) {
                 <title>Starbots | The first-ever robot battle NFT game</title>
                 <link rel="preconnect" href="https://fonts.googleapis.com"/>
                 <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={"true"}/>
-                <link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet"/>
             </Head>
             {isMobile ? (
                 <Mobile/>
